a50a94b56aa535c021054434c008c17a66f3b88a
Zhao <luyi.zhao@sap.com>
2019-03-11 10:03:50 +0800

    test

--- src/services/pkg.ts
+++ src/services/pkg.ts
@@ -1,23 +1,3 @@
 import { getManager } from 'typeorm'
 import { Package } from '../models/package'
 import { PackageT } from '../models/packageT'
-const getFeedbacks = async () => {
-  const feedbackRepo = getManager().getRepository(Feedback)
-  return feedbackRepo.find()
-}
-const addFeedback = async (text: string) => {
-  const feedbackRepo = getManager().getRepository(Feedback)
-  const feedback: Feedback = new Feedback()
-  feedback.text = text
-  return feedbackRepo.save(feedback)
-}
-const removeFeedback = async (id: number) => {
-  const feedbackRepo = getManager().getRepository(Feedback)
-  const feedback = await feedbackRepo.findOne(id)
-  return feedbackRepo.remove(feedback)
-}
-export default {
-  getFeedbacks,
-  addFeedback,
-  removeFeedback
-}
